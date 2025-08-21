4ffef7dff133a50f565bad9e19684127c743bdc4
guillemyo1 <guillemyo1@gmail.com>
2025-08-21 14:53:21 +0000

    mas pequeños

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -107,7 +107,7 @@ export function Sidebar() {
                                     key={item.href}
                                     href={item.href}
                                     className={cn(
-                                        "flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:bg-primary-foreground/10 text-shadow-sm shadow-black/20",
+                                        "flex items-center gap-2 rounded-lg px-3 py-1.5 transition-all hover:bg-primary-foreground/10 text-shadow-sm shadow-black/20",
                                         isActive
                                         ? "bg-primary-foreground/10"
                                         : "hover:text-primary-foreground"
