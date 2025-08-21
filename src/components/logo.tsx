ba17d1b6b1861d3ec285453b67f1298b4f6bb9d4
guillemyo1 <guillemyo1@gmail.com>
2025-08-21 13:38:48 +0000

    ahora que se parta en diagonal los colores del logo, no en vertical

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -10,8 +10,8 @@ export function Logo() {
       xmlns="http://www.w3.org/2000/svg"
       className="h-6 w-6"
     >
-      <path d="M0 4C0 1.79086 1.79086 0 4 0H12V24H4C1.79086 24 0 22.2091 0 20V4Z" fill="#2563eb" />
-      <path d="M12 0H20C22.2091 0 24 1.79086 24 4V20C24 22.2091 22.2091 24 20 24H12V0Z" fill="#34D399" />
+      <path d="M0 24 L24 0 H0 V24Z" fill="#34D399" />
+      <path d="M24 0 V24 H0 L24 0Z" fill="#2563eb" />
     </svg>
   );
 }
