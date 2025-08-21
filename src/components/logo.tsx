4c58cc5af252ec957879ac1e45ce735ea33bb9b0
guillemyo1 <guillemyo1@gmail.com>
2025-08-21 19:38:43 +0000

    este

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,18 +1,25 @@
 import * as React from 'react';
-import Image from 'next/image';
 
 export function Logo() {
-  const logoUrl =
-    'https://firebasestorage.googleapis.com/v0/b/sportspanel.firebasestorage.app/o/SportsPanel_logo_no_fondo.png?alt=media';
-
   return (
-    <Image
-      src={logoUrl}
-      alt="SportsPanel Logo"
-      width={42}
-      height={42}
-      className="h-10 w-10"
-      unoptimized
-    />
+    <svg
+      width="42"
+      height="42"
+      viewBox="0 0 100 100"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <defs>
+        <clipPath id="rounded-corners">
+          <rect width="100" height="100" rx="20" ry="20" />
+        </clipPath>
+      </defs>
+      <g clipPath="url(#rounded-corners)">
+        <rect width="100" height="100" fill="#34d399" />
+        <path
+          d="M 0,50 C 20,40 50,85 100,70 L 100,100 L 0,100 Z"
+          fill="#3b82f6"
+        />
+      </g>
+    </svg>
   );
 }
