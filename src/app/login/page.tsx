d06da336280ec425c8c263a5ecd0522147711f17
guillemyo1 <guillemyo1@gmail.com>
2025-08-22 14:44:53 +0000

    starting build "e6284cb1-9439-4985-9bbe-7db287b28573" FETCHSOURCE BUILD

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,6 +11,7 @@ import {
   CardDescription,
   CardFooter,
   CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
