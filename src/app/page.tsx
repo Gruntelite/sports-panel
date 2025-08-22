c9ebf116c122e6357bac3091347bace97ecf5300
guillemyo1 <guillemyo1@gmail.com>
2025-08-22 13:25:46 +0000

    por favor centra el texto del subtitulo y reduce y haz salto de linea el

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -171,9 +171,9 @@ export default function LandingPage() {
                  <div className="flex flex-col items-center justify-center space-y-4 text-center mb-12">
                     <div className="space-y-2">
                         <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl font-headline">
-                           Ahorra tiempo, reduce errores y mejora la comunicación
+                           Ahorra tiempo, reduce errores <br/> y mejora la comunicación
                         </h2>
-                        <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                        <p className="max-w-[900px] text-muted-foreground text-center text-lg/relaxed lg:text-base/relaxed xl:text-lg/relaxed">
                             SportsPanel automatiza las tareas repetitivas para que puedas centrarte en lo que de verdad importa: el deporte.
                         </p>
                     </div>
