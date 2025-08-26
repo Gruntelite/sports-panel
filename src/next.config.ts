5a3b2e776160735788da25ea289fea8459b03467
guillemyo1 <guillemyo1@gmail.com>
2025-08-26 13:42:56 +0000

    pero has visto esto?

---
+++ src/next.config.ts
@@ -0,0 +1,36 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  serverActions: {
+    bodySizeLimit: '15mb',
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      }
+    ],
+  },
+  devIndicators: {
+    allowedDevOrigins: [
+        '*.cloudworkstations.dev',
+    ]
+  },
+};
+
+export default nextConfig;
